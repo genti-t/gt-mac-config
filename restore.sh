2ab3a817e2e3a8a1b748f5dd92d32e3e50d58e43
Genti Topija <genti.topija@banzai.it>
2016-08-02 10:08:09 +0200

    remove restore file

--- restore.sh
+++
@@ -1,8 +0,0 @@
-
-# ATOM
-rsync -rav Users/genti/.atom/config.cson ~/.atom/
-rsync -rav Users/genti/.atom/styles.less  ~/.atom/
-rsync -rav Users/genti/.atom/packages  ~/.atom/
-
-# iTerm (iTerm must be closed when copy file)
-cp Preferences/com.googlecode.iterm2.plist ~/Library/Preferences/
